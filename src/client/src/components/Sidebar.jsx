a52c8c776c56b07cd2407688718bc20a893dbaff
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-08-19 07:10:19 +0100

    Some UI updates (#21)

--- src/client/src/components/Sidebar.jsx
+++ src/client/src/components/Sidebar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import "./Sidebar.css";
 import QuizOutlinedIcon from "@mui/icons-material/QuizOutlined";
 import LeaderboardOutlinedIcon from "@mui/icons-material/LeaderboardOutlined";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import Avatar from "boring-avatars";
+import { useAuth } from "../context/AuthContext";
 
 const AccountSection = ({ userName }) => {
   const [isDropdownVisible, setDropdownVisible] = useState(false);
@@ -16,20 +18,20 @@ const AccountSection = ({ userName }) => {
     >
       <div className="user-info">
         <div className="user-avatar">
-          <img
-            src="https://via.placeholder.com/40"
-            alt="User Avatar"
-            style={{ borderRadius: "50%" }}
+          <Avatar
+            name={userName}
+            size={40}
+            colors={["#fb6900", "#f63700", "#004853", "#007e80", "#00b9bd"]}
           />
         </div>
         <span className="user-name">{userName}</span>
       </div>
       {isDropdownVisible && (
         <ul className="dropdown-menu">
-          <Link href="/settings" passHref>
+          <Link to="/settings">
             <li>Account Settings</li>
           </Link>
-          <Link href="/logout" passHref>
+          <Link to="/logout">
             <li>Log Out</li>
           </Link>
         </ul>
@@ -39,31 +41,45 @@ const AccountSection = ({ userName }) => {
 };
 
 function Sidebar() {
+  const { user } = useAuth();
+
   return (
     <nav className="sidebar">
       <div className="main-links-container">
         <ul>
           <li>
-            <a href="#" className="menu-item">
+            <a href="/" className="menu-item">
+              <InfoOutlinedIcon className="menu-icon" />
+              <span>Home</span>
+            </a>
+          </li>
+          <li>
+            <a href="/dashboard" className="menu-item">
+              <InfoOutlinedIcon className="menu-icon" />
+              <span>Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a href="/quiz" className="menu-item">
               <QuizOutlinedIcon className="menu-icon" />
               <span>Quizzes</span>
             </a>
           </li>
           <li>
-            <a href="#" className="menu-item">
+            <a href="/leaderboard" className="menu-item">
               <LeaderboardOutlinedIcon className="menu-icon" />
               <span>Leaderboard</span>
             </a>
           </li>
           <li>
-            <a href="#" className="menu-item">
+            <a href="/about" className="menu-item">
               <InfoOutlinedIcon className="menu-icon" />
               <span>How it works</span>
             </a>
           </li>
         </ul>
       </div>
-      <AccountSection userName="John Doe" />
+      {user && <AccountSection userName={user.name} />}
     </nav>
   );
 }
