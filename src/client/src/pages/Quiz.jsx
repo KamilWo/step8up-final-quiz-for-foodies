8cfd2bfb50da12a075912c69e882c7d3e8e8c2cc
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-08-19 17:27:44 +0100

    Added randomness to the Quiz page

--- src/client/src/pages/Quiz.jsx
+++ src/client/src/pages/Quiz.jsx
@@ -1,20 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Question from "../components/Question";
 import data from "../../../server/seeds/quizzes_with_options.json";
+
 export default function Quiz() {
   const [question, setQuestions] = useState(data);
   const [highscore, setHighscore] = useState(300);
 
+  function random_question() {
+    // Data not empty
+    if (data.length === 0) {
+      console.warn("No quiz data available to select a random question.");
+      return; // Set a default/null state for question
+    }
+    const randomIndex = Math.floor(Math.random() * data.length);
+    setQuestions(data[randomIndex]); // Update the state with the randomly selected question
+  }
+
+  // Call random_question when the component mounts
+  useEffect(() => {
+    random_question();
+  }, []); // Empty dependency array ensures this runs only once on mount
+
   return (
     <>
       <Question
-        question={question[0].question}
-        category={question[0].category}
+        question={question.question}
+        category={question.category}
         highscore={highscore}
-        option1={question[0].option_01}
-        option2={question[0].option_02}
-        option3={question[0].option_03}
-        option4={question[0].option_04}
+        option1={question.option_01}
+        option2={question.option_02}
+        option3={question.option_03}
+        option4={question.option_04}
       ></Question>
     </>
   );
