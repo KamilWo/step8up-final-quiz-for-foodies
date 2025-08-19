bf647d08fdf1b4dd72b3aa93786350c2b00b9c15
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-08-19 20:06:05 +0100

    Ranks added and api endpoints, and seeding (#25)
    * Ranks added and api endpoints, and seeding
    
    * Fixed users

--- src/server/models/index.mjs
+++ src/server/models/index.mjs
@@ -1,2 +1,18 @@
-export { default as User } from "./user.mjs";
-export { default as Quiz } from "./quiz.mjs";
+// export { default as User } from "./user.mjs";
+// export { default as Quiz } from "./quiz.mjs";
+
+import User from "./user.mjs";
+ import Quiz from "./quiz.mjs";
+ import Rank from "./rank.mjs";
+
+ // Model associations
+ User.hasMany(Rank, {
+   foreignKey: "user_id",
+   onDelete: "CASCADE", // If a user is deleted, their ranks are also deleted
+ });
+
+ Rank.belongsTo(User, {
+   foreignKey: "user_id",
+ });
+
+ export { User, Quiz, Rank };
